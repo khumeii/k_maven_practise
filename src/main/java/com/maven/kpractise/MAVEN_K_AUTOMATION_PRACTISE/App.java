a775728a30600808f59422f9076921080759d356
khumeii <khurramafzal@gmx.com>
2021-12-19 03:59:50 -0500

    Fist commit

---
+++ src/main/java/com/maven/kpractise/MAVEN_K_AUTOMATION_PRACTISE/App.java
@@ -0,0 +1,13 @@
+package com.maven.kpractise.MAVEN_K_AUTOMATION_PRACTISE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/maven/kpractise/MAVEN_K_AUTOMATION_PRACTISE/AppTest.java
@@ -0,0 +1,20 @@
+package com.maven.kpractise.MAVEN_K_AUTOMATION_PRACTISE;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
