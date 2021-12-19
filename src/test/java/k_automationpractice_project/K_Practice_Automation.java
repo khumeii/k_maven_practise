b4bde59e2046da24be3eb7bc7fdfd24dc96b19e3
khumei <khurramafzal@gmx.net>
2021-12-19 17:05:50 -0500

    Third commit

--- src/test/java/k_automationpractice_project/K_Practice_Automation.java
+++ src/test/java/k_automationpractice_project/K_Practice_Automation.java
@@ -1,5 +1,50 @@
 package k_automationpractice_project;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class K_Practice_Automation {
 
+	public static WebDriver driver;
+	public static ChromeOptions options = new ChromeOptions();
+	public static WebDriverWait wait;
+	public static SoftAssert softassert = new SoftAssert();
+	
+	@BeforeMethod
+	public void initialize() {
+	
+			WebDriverManager.chromedriver().setup();
+			options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+			options.addArguments("--start-maximized");
+			options.addArguments("--incognito");
+			driver = new ChromeDriver(options);
+			driver.get("http://automationpractice.com/index.php");
+	}
+	
+	@Test
+	public void clickonSignInLink() {
+		
+		wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Sign in")));
+		driver.findElement(By.linkText("Sig in")).click();
+
+	}
+	
+	@AfterMethod
+		public void tearDown() {
+//		driver.quit();
+		
+	}
+			
 }
